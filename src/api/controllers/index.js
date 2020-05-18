37dcf942885452838359024094817621183d6b55
caiovn <caiovnasci@gmail.com>
2020-05-18 16:14:11 -0300

    refactor: controller files

---
+++ src/api/controllers/index.js
@@ -0,0 +1,53 @@
+module.exports = () => {
+    const controller = {};
+    const client = require('../../database/client')();
+
+    client.connect();
+
+    controller.listPokemons = (req, res) => {
+        const text = 'SELECT info FROM pokemons'
+
+        client.query(text, (err, result) => {
+            if(err){
+                console.log(err);
+                res.status(400).send(err);
+            } else {
+                res.status(200).json(result.rows);
+            }
+        });
+    };
+
+    controller.getPokemonById = (req, res) => {
+        const { id } = req.params;
+
+        const text = 'SELECT info FROM pokemons WHERE id=$1';
+        const value = [id];
+
+        client.query(text, value, (err, result) => {
+            if(err){
+                console.log(err);
+                res.status(400).send(err);
+            } else {
+                res.status(200).json(result.rows);
+            }
+        })
+    };
+
+    controller.getPokemonByName = (req, res) => {
+        const { name } = req.params;
+
+        const text = "SELECT info FROM pokemons WHERE info ->> 'name' ILIKE $1";
+        const value = [name];
+
+        client.query(text, value, (err, result) => {
+            if(err) {
+                console.log(err);
+                res.status(400).send(err);
+            } else {
+                res.status(200).json(result.rows);
+            }
+        });
+    };
+
+    return controller;
+}
--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,9 +1,7 @@
 module.exports = app => {
-    const pokemons = require('../controllers/pokemons')();
-    const idPokemon = require('../controllers/idPokemon')();
-    const namePokemon = require('../controllers/namePokemon')();
+    const controller = require('../controllers/index')(app);
 
-    app.route('/api/v1/pokemons').get(pokemons.listPokemons);
-    app.route('/api/v1/pokemon/id/:id').get(idPokemon.getPokemonId);
-    app.route('/api/v1/pokemon/name/:name').get(namePokemon.pokemonByName);
+    app.route('/api/v1/pokemons').get(controller.listPokemons);
+    app.route('/api/v1/pokemon/id/:id').get(controller.getPokemonById);
+    app.route('/api/v1/pokemon/name/:name').get(controller.getPokemonByName);
 }
